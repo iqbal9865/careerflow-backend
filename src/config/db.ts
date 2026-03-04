84adce71e79857f34fb14ce6deabc6ecee0eec0a
iqbal9865 <iqbalahmed9865@gmail.com>
2026-03-04 00:55:33 +0000

    access token generate on sign up for minimize login func after registration

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,7 +7,6 @@ dotenv.config();
 console.log("✅ db.ts file loaded")
 console.log("DATABASE_URL:", process.env.DATABASE_URL)
 
-
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 
 export const prisma = new PrismaClient({ adapter })
--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,6 @@ app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
